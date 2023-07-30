4d24de01373f887f9355af361657a2c0d2d1614a
Nathaniel Burke <nrb21@ic.ac.uk>
2023-07-30 17:14:46 +0100

    Fix ligature button
    For some reason the style only seems to apply to `span`s, not `code`
    elements, so we nest a `span` inside every `code` element on page load
    and update the style of those.

--- theme/ligature_toggle.js
+++ theme/ligature_toggle.js
@@ -7,18 +7,28 @@ const buttonText = () =>
   `${ligatures_enabled ? "Disable" : "Enable"} Ligatures`;
 
 let ligatures_enabled = true;
+
+Array.from(document.getElementsByTagName("code")).forEach((code_block) => {
+  let span = document.createElement("span");
+  span.innerText = code_block.innerText;
+  code_block.innerText = "";
+  code_block.appendChild(span);
+});
+
 const button = document.createElement("button");
 button.innerText = buttonText();
 button.addEventListener("click", () => {
   ligatures_enabled = !ligatures_enabled;
   button.innerText = buttonText();
-  Array.from(document.getElementsByClassName("language-wty2 hljs")).forEach(
-    (code_block) => {
-      console.log(code_block);
-      code_block.style.fontFamily = "yeow";
-    }
-  );
+
   if (ligatures_enabled == false) {
+    Array.from(document.getElementsByTagName("code")).forEach((code_block) => {
+      code_block.children[0].style.fontFamily = "Fira Mono Google";
+    });
+  } else {
+    Array.from(document.getElementsByTagName("code")).forEach((code_block) => {
+      code_block.children[0].style.fontFamily = "";
+    });
   }
 });
 document.getElementsByClassName("left-buttons")[0].appendChild(button);
